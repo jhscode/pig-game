dbbb07afe539159d340d97f7f44791b27dfe3562
James Shin <jhscode@gmail.com>
2018-03-18 17:27:16 -0400

    update round score & toggle players

--- app.js
+++ app.js
@@ -34,11 +34,31 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 
 
   // 3. Update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  } else {
+    // Next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
 
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-0').textContent = 1;
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    //document.querySelector('.player-0-panel').classList.remove('active');
+    //document.querySelector('.player-1-panel').classList.add('active');
+
+    document.querySelector('.dice').style.display = 'none';
+
+  }
 
 
 });
 
 
-// document.querySelector('#current-' + activePlayer).textContent = dice;
+
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
